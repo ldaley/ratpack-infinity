c86cb3268611d8e1829c926be12cba946714dd48
Luke Daley <ld@ldaley.com>
2017-12-08 12:56:58 +1000

    Fix infinite recursion.

--- src/main/java/infinity/InfinityRecursionAction.java
+++ src/main/java/infinity/InfinityRecursionAction.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ratpack.exec.Blocking;
 import ratpack.exec.Execution;
+import ratpack.exec.Operation;
 import ratpack.exec.Promise;
 import ratpack.func.Action;
 
@@ -18,28 +19,25 @@ public class InfinityRecursionAction implements Action<Execution> {
 
     @Override
     public void execute(Execution execution) throws Exception {
-        poll()
-            .result(r -> {
-                Throwable error = r.getThrowable();
-                LOG.error("Unexpected error", error);
-            });
+        Operation.of(this::loop)
+            .onError(e -> LOG.error("Unexpected error", e))
+            .then();
     }
 
-    private Promise<Void> poll() {
-        return getFoo()
-            .flatMap(this::useFoo)
-            .flatMap(v -> this.poll());
+    private void loop() {
+        poll().then(this::loop);
+    }
+
+    private Operation poll() {
+        return getFoo().operation(this::useFoo);
     }
 
     private Promise<Foo> getFoo() {
         return Blocking.get(Foo::new);
     }
 
-    private Promise<Void> useFoo(Foo foo) {
-        return Blocking.get(() -> {
-            LOG.debug("foo={}", foo.name);
-            return null;
-        });
+    private void useFoo(Foo foo) {
+        Blocking.exec(() -> LOG.debug("foo={}", foo.name));
     }
 
     private static class Foo {
